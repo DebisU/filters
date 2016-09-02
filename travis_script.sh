c1fa84d9181363a6c0727387c7df0d46e5d64c50
Xavier Fornes <xavier.fornes@scmspain.com>
2016-09-02 07:44:05 +0200

    explain what we are doing in travis_script

--- travis_script.sh
+++ travis_script.sh
@@ -5,5 +5,6 @@ set -xe
 ./gradlew check
 
 if [[ ${TRAVIS_BRANCH} = "master" && ${TRAVIS_PULL_REQUEST} != "false" ]]; then
+    # Publicamos una versión beta para a esa branch
     ./gradlew publish -Ppr.number=${TRAVIS_PULL_REQUEST}
 fi
