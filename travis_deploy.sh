9fcb8f3892b11e7f4aa379bf8eb27ae79b174848
Xavier Fornes <xavier.fornes@scmspain.com>
2016-09-02 08:12:06 +0200

    integration of checkstyle, pmd, findbugs and sonarqube

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,5 +2,9 @@
 
 set -xe
 
+# Publish to sonarqube
+./gradlew sonarqube -Dsonar.host.url=https://sonarqube.spain.schibsted.io -Dsonar.login=${SONAR_TOKEN} -Dsonar.analysis.mode=publish
+
+# Publish release
 ./gradlew release -Prelease.disableRemoteCheck -Prelease.pushTagsOnly  -Prelease.disableUncommittedCheck
 ./gradlew publish
--- travis_script.sh
+++ travis_script.sh
@@ -2,7 +2,10 @@
 
 set -xe
 
-./gradlew check
+#./gradlew check
+# Skipping checkstyle, findbug and pmd temporarily
+./gradlew check -x checkstyleMain -x checkstyleTest -x findbugsMain -x findbugsTest -x pmdMain -x pmdTest
+
 
 if [[ ${TRAVIS_BRANCH} = "master" && ${TRAVIS_PULL_REQUEST} != "false" ]]; then
     # Publicamos una versión beta para a esa branch
